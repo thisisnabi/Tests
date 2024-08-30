27100606b095ce5c25b0108ac7043b2d313b7584
Nabi Karampoor <thisisnabi@outlook.com>
2024-08-30 22:28:40 +0330

    Add loadBalancer UnitTests

---
+++ UnitTests/Tests.Src.UnitTests/Example1/LoadBalancer.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Tests.Src.UnitTests.Example1;
+public class LoadBalancer
+{
+    private readonly string[] _connections;
+    private int currentIndex = -1;
+    private const int charOffset = 97;
+
+    public LoadBalancer(string[] connections)
+    {
+        _connections = connections;
+    }
+
+    public int TenantId => currentIndex;
+    public string TenantConnectionString => _connections[currentIndex];
+
+    public Tenant MoveNext()
+    {
+        currentIndex++;
+        if (currentIndex > 2)
+            currentIndex = 0;
+
+        var currentChar = (char)(charOffset + currentIndex);
+        return new Tenant(currentIndex + 1, _connections[currentIndex], currentChar);
+    }
+
+    internal string GetConnectionStringByPredicateId(char v)
+    {
+        var wordIndex = (int)v;
+        var index = wordIndex - charOffset;
+
+        //if (index >= _connections.Length)
+        //    throw new IndexOutOfRangeException(nameof(_connections));
+
+        return _connections[index];
+    }
+
+    public IEnumerable<string> GetConnections() => _connections;
+}
+
+public record Tenant(int Id, string ConnectionString, char Predicate);
---
+++ UnitTests/Tests.UnitTests/Example1/LoadBalancerTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using Tests.Src.UnitTests.Example1;
+
+namespace Tests.UnitTests.Example1;
+
+public class LoadBalancerTests
+{
+    private readonly LoadBalancer _sut;
+
+    // setup
+    public LoadBalancerTests()
+    {
+        _sut = new LoadBalancer(["conn1", "conn2", "conn3"]);
+    }
+
+
+    [Fact]
+    public void MoveNext_ShouldRotateThroughConnections()
+    {
+        // act
+        var tenant1 = _sut.MoveNext();
+        var tenant2 = _sut.MoveNext();
+        var tenant3 = _sut.MoveNext();
+        var tenant4 = _sut.MoveNext();
+
+        // assert
+        tenant1.ConnectionString.Should().Be("conn1");
+        tenant1.Id.Should().Be(1);
+        tenant1.Predicate.Should().Be('a');
+
+        tenant2.ConnectionString.Should().Be("conn2");
+        tenant2.Id.Should().Be(2);
+        tenant2.Predicate.Should().Be('b');
+
+        tenant3.ConnectionString.Should().Be("conn3");
+        tenant3.Id.Should().Be(3);
+        tenant3.Predicate.Should().Be('c');
+
+        tenant4.ConnectionString.Should().Be("conn1");
+        tenant4.Id.Should().Be(1);
+        tenant4.Predicate.Should().Be('c');
+    }
+
+    [Theory]
+    [InlineData('a', "conn1")]
+    [InlineData('b', "conn2")]
+    [InlineData('c', "conn3")]
+    public void GetConnectionStringByPredicateId_ShouldReturnCorrectConnectionString(char act, string expected)
+    {
+        // act
+        var conn = _sut.GetConnectionStringByPredicateId(act);
+
+        // assert
+        conn.Should().Be(expected);   
+    }
+
+    [Fact]
+    public void GetConnectionStringByPredicateId_ShouldThrowExceptionInMaxIndex()
+    {
+        // act
+        var connFunc = () => _sut.GetConnectionStringByPredicateId('d');
+
+        // assert
+        connFunc.Should().Throw<IndexOutOfRangeException>();
+    }
+}
