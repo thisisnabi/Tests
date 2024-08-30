45d13e6eab527f88db03029290dd9ffa03111e1e
Nabi Karampoor <thisisnabi@outlook.com>
2024-08-31 00:24:32 +0330

    Add Callgroup Test

---
+++ UnitTests/Tests.Src.UnitTests/Examples/CallGroup.cs
@@ -0,0 +1,67 @@
+﻿namespace Tests.Src.UnitTests.Examples;
+
+public class CallGroup<TOperation>
+{
+    private readonly int initialParticipantCount;
+    private readonly Func<IReadOnlyCollection<TOperation>, Task> groupCallDelegate;
+    private readonly TimeSpan timeout;
+    private readonly List<TOperation> requests;
+    private readonly Task task;
+    private readonly object @lock = new();
+    private readonly TaskCompletionSource barrier;
+    private int joinedParticipantCount;
+    public CallGroup(int participantCount,
+    Func<IReadOnlyCollection<TOperation>, Task> groupCallDelegate,
+    TimeSpan timeout)
+    {
+        if (participantCount <= 0)
+        {
+            throw new ArgumentException($"Value {participantCount} should be greater than zero!", nameof(participantCount));
+        }
+        joinedParticipantCount = 0;
+        initialParticipantCount = participantCount;
+        this.groupCallDelegate = groupCallDelegate;
+        this.timeout = timeout;
+        requests = new();
+        barrier = new();
+        task = Task.Run(WaitParticipants);
+    }
+    public Task Join(TOperation value)
+    {
+        ProcessNewJoiner(() => requests.Add(value));
+        return task;
+    }
+    private async Task WaitParticipants()
+    {
+        try
+        {
+            await barrier.Task.WaitAsync(timeout);
+        }
+        catch (Exception err)
+        {
+            throw new Exception(err.Message, err);
+        }
+        await groupCallDelegate(requests);
+    }
+    public void Leave()
+    {
+        ProcessNewJoiner(() => { });
+    }
+    private void ProcessNewJoiner(Action addState)
+    {
+        lock (@lock)
+        {
+            var leftToJoin = initialParticipantCount - joinedParticipantCount;
+            if (leftToJoin <= 0)
+            {
+                throw new Exception("Too many participants!");
+            }
+            joinedParticipantCount++;
+            addState();
+            if (leftToJoin == 1)
+            {
+                barrier.SetResult();
+            }
+        }
+    }
+}
---
+++ UnitTests/Tests.UnitTests/Example5/CallGroupTests.cs
@@ -0,0 +1,100 @@
+﻿using FluentAssertions;
+using Tests.Src.UnitTests.Examples;
+using Xunit.Abstractions;
+
+namespace Tests.UnitTests.Example5;
+
+public class CallGroupTests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public CallGroupTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+
+    }
+
+    [Fact]
+    public void Constractor_ShouldThrowArgumentException_WhenParticipantIsZeroOrNegative()
+    {
+        Func<IReadOnlyCollection<string>, Task> func = _ => Task.FromResult(0);
+
+        var funcWithZero = () => new CallGroup<string>(0, func, TimeSpan.FromSeconds(1));
+        var funcWithNegative = () => new CallGroup<string>(-1, func, TimeSpan.FromSeconds(1));
+
+        funcWithZero.Should().Throw<ArgumentException>().WithMessage("Value 0 should be greater than zero! (Parameter 'participantCount')");
+        funcWithNegative.Should().Throw<ArgumentException>().WithMessage("Value -1 should be greater than zero! (Parameter 'participantCount')");
+    }
+
+
+    [Fact]
+    public async Task Join_ToMAny()
+    {
+        var students = new List<string>();
+        var func = new Func<IReadOnlyCollection<string>, Task>((requests) =>
+        {
+            students.AddRange(requests);
+            return Task.CompletedTask;
+        });
+
+        var group = new CallGroup<string>(3, func, TimeSpan.FromSeconds(3));
+
+        var t1 = group.Join("Helia");
+        var t2 = group.Join("Samane");
+        var t3 = group.Join("Nader");
+    
+        var action = async () => await group.Join("Sheeva");;
+        await action.Should().ThrowAsync<Exception>().WithMessage("Too many participants!");
+
+    }
+
+
+    [Fact]
+    public async Task Join()
+    {
+        var students = new List<string>();
+        var expectedStudents = new List<string>() { "Helia", "Samane", "Nader" };
+        var func = new Func<IReadOnlyCollection<string>, Task>((requests) =>
+        {
+            students.AddRange(requests);
+            return Task.CompletedTask;
+        });
+
+        var group = new CallGroup<string>(3, func, TimeSpan.FromSeconds(3));
+
+        var t1 = group.Join("Helia");
+        var t2 = group.Join("Samane");
+        var t3 = group.Join("Nader");
+
+       await Task.WhenAll(t1, t2, t3);
+
+        students.Should().HaveCount(3);
+        students.Should().BeEquivalentTo(expectedStudents);
+    }
+
+
+
+    [Fact]
+    public async Task Timeout()
+    {
+        var students = new List<string>();
+        var expectedStudents = new List<string>() { "Helia", "Samane", "Nader" };
+        var func = new Func<IReadOnlyCollection<string>, Task>((requests) =>
+        {
+            students.AddRange(requests);
+            return Task.CompletedTask;
+        });
+
+        var callGroup = new CallGroup<string>(3, func, TimeSpan.FromMilliseconds(10));
+
+        await Task.Delay(TimeSpan.FromSeconds(1));
+       
+        var action = async () => await callGroup.Join("Nabi");
+          
+        await action.Should().ThrowAsync<Exception>().WithMessage("The operation has timed out.");
+
+    }
+
+
+
+}
