763e1b1f7993fdb9cf6a612d4647d85440911157
Nabi Karampoor <thisisnabi@outlook.com>
2024-08-30 23:21:14 +0330

    Add paged list tests

---
+++ UnitTests/Tests.Src.UnitTests/Example4/PagingParams.cs
@@ -0,0 +1,45 @@
+﻿namespace Tests.Src.UnitTests.Example4;
+ 
+public class PagedList<T>
+{
+    private PagedList(
+        int totalItems,
+        IReadOnlyCollection<T> items,
+        int pageNumber,
+        int pageSize)
+    {
+        TotalItems = totalItems;
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        Items = items;
+    }
+
+    public static async Task<PagedList<T>> Paginate(
+        IQueryable<T> source,
+        int pageNumber,
+        int pageSize,
+        CancellationToken cancellationToken =default)
+    {
+        var totalItems = source.Count();
+        var items = source
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        return new PagedList<T>(totalItems, items, pageNumber, pageSize);
+    }
+
+    public int TotalItems { get; }
+    public int PageNumber { get; }
+    public int PageSize { get; }
+    public IReadOnlyCollection<T> Items { get; }
+    public int TotalPages =>
+          (int)Math.Ceiling(TotalItems / (double)PageSize);
+    public bool HasPreviousPage => PageNumber > 1;
+    public bool HasNextPage => PageNumber < TotalPages;
+    public int NextPageNumber =>
+           HasNextPage ? PageNumber + 1 : TotalPages;
+    public int PreviousPageNumber =>
+           HasPreviousPage ? PageNumber - 1 : 1;
+}
+ 
---
+++ UnitTests/Tests.UnitTests/Example4/PagedListTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Tests.Src.UnitTests.Example4;
+
+namespace Tests.UnitTests.Example4;
+
+public class PagedListTests
+{
+    [Fact]
+    public async Task Constractor_ShouldInitilizeProperties()
+    {
+        // arrange
+        var items = new List<string> { "item1", "item2", "item3" };
+        var expecteditems = new List<string> { "item1", "item2" };
+
+        var pagedList = await PagedList<string>.Paginate(
+            items.AsQueryable(),
+            1,
+            2,
+            CancellationToken.None);
+ 
+        pagedList.TotalItems.Should().Be(3);
+        pagedList.Items.Should().HaveCount(2);  
+        pagedList.PageNumber.Should().Be(1);
+        pagedList.PageSize.Should().Be(2);
+        pagedList.HasNextPage.Should().BeTrue();
+        pagedList.HasPreviousPage.Should().BeFalse();
+
+        pagedList.Items.Should().BeEquivalentTo(expecteditems);
+    }
+
+
+}
